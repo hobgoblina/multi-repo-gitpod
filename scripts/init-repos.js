02f3caa116da7ae4c68b84eecd8a3e22187057f8
Lina <lina.reyne@gmail.com>
2022-01-12 02:26:12 +0000

    add npm install

--- scripts/init-repos.js
+++ scripts/init-repos.js
@@ -6,6 +6,8 @@ const yaml = require('yaml');
 const fs = require('fs');
 
 (async () => {
-  const configs = yaml.parse(fs.readFileSync('repos.yml').toString());
-  configs.repos.forEach(async repo => await gitClone(repo.url, `/workspace/${repo.name}`, { checkout: repo.branch }));
+  const { repos } = yaml.parse(fs.readFileSync('repos.yml').toString());
+  for (const repo of repos) {
+    await gitClone(repo.url, `/workspace/${repo.name}`, { checkout: repo.branch });
+  }
 })();
